c6511ecabe469acaa1efca5da297e520a6c61373
EllaNancyFay <ellaschofield0@gmail.com>
2014-10-16 12:07:35 +0100

    Wrote airport accept plane test

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -4,7 +4,13 @@ describe('The airport', function(){
 
 		it('it should have a planes array',function(){
 			airport = new Airport;
-			expect(airport.planes).toEqual([]);
+			expect(airport.planes instanceof Array).toBe(true)
+		});
+
+		it('should accept a plane', function(){
+			airport = new Airport;
+			airport.land(plane);
+			expect(airport.planes.length).toEqual(1)
 		});
 	});
 });
--- src/airport.js
+++ src/airport.js
@@ -1,3 +1,4 @@
 function Airport(){
 	this.planes = [];
 };
+
