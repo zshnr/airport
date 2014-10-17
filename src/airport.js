6bf87c998069c82fd593f5bb9889acbd4f9b0b53
Zeeshan Rasool <zrasool88@gmail.com>
2014-10-17 14:22:59 +0100

    take off function can only allow an instance of plane to take off once

--- src/airport.js
+++ src/airport.js
@@ -14,19 +14,13 @@ Airport.prototype.land = function(plane) {
 		return 'Plane has already landed'
 };
 
-Airport.prototype.takeOff = function(plane) {
-	this.planes.splice(this.planes.indexOf(plane),1)[0];
+Airport.prototype.takeOff = function(planes) {
+	if(this.planes.indexOf(plane)!== -1)
+		this.planes.splice(this.planes.indexOf(plane),1)[0];
+	else
+		return 'Plane has already taken off'
 };
 
 Airport.prototype.checkIn = function(passenger) {
 	this.passengers.push(passenger);
 };
-
-// The code can allow the same plane to land multiple times.
-// This needs to be fixed
-// Need to understand this code :
-	
-		// Airport.prototype._clearToTakeOff = function(plane) {
-		// return this._clearManyPlanesToTakeOff(plane, 1)[0];
-// Why does, after passing two arguments do we need to give it [0]?
-// is that to return? to splice?
